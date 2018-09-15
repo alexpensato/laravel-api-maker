7cc4ef4e9b735c2076ffa9a298b612f3d26a81ac
Alex Magalhaes <alex@pensato.com.br>
2018-09-15 18:32:40 -0300

    Unused files removed

--- templates/Http/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Pensato\Api\Support\ApiController;
-
-abstract class Controller extends ApiController
-{
-}
--- templates/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'v1'], function () {
-    //
-});
