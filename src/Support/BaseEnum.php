d120f3032224f88a6a6df0e365bee7cff02222fd
Ghabriel Calsa Nunes <gcnunes@SEF.SC.GOV.BR>
2018-11-13 13:29:47 -0200

    Added a BaseEnum implementation

---
+++ src/Support/BaseEnum.php
@@ -0,0 +1,35 @@
+<?php
+
+class BaseEnum {
+	static function init() {
+		$className = get_called_class();
+		$class = new ReflectionClass($className);
+		$properties = $class->getStaticProperties();
+		$counter = 0;
+
+		foreach ($properties as $name => $value) {
+			$className::$$name = new $className($counter);
+			$counter++;
+		}
+	}
+
+	static function fromInt(int $value) {
+		$className = get_called_class();
+		$class = new ReflectionClass($className);
+		$properties = $class->getStaticProperties();
+
+		if ($value < 0 || $value >= count($properties)) {
+			throw new Exception("Invalid enum value");
+		}
+		
+		$property = array_keys($properties)[$value];
+
+		return $className::$$property;
+	}
+	
+	private function __construct($value) {
+		$this->value = $value;
+	}
+
+	private $value;
+}
